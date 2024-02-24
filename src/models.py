9222aff1fa46b46c0b0ef29423f362fb08a8a757
Fran Julian <138630369+Franjjr@users.noreply.github.com>
2024-02-24 20:54:26 +0000

    FEAT: se han creado los endpoint y en teoria la base del ejercicio esta terminada a falta de que se nos pida algo mas

--- src/models.py
+++ src/models.py
@@ -20,3 +20,37 @@ class Users(db.Model):
             "email": self.email,
             'pass': self.password,
             'is_active': self.is_active}
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    is_favorite = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.planet
+
+    def serialize(self):
+        # do not serialize the password, its a security breach
+        return {
+            "id": self.id,
+            "name": self.name,
+            'is_favorite': self.is_favorite}
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    is_favorite = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.people
+
+    def serialize(self):
+        # do not serialize the password, its a security breach
+        return {
+            "id": self.id,
+            "firstname": self.first_name,
+            "lastname": self.last_name,
+            "email": self.email,
+            'is_favorite': self.is_favorite}
